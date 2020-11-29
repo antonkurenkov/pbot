b5253bf950267b0f3bca9b3c78aa9425fc6e28f7
antonkurenkov <a.k.a1306@gmail.com>
2020-11-30 02:28:25 +0300

    setup.sh

--- setup.sh
+++ setup.sh
@@ -1,24 +1,24 @@
-# INSTALL CHROME
 sudo apt-get -y update
 sudo apt-get install -y gconf-service libasound2 libatk1.0-0 libcairo2 libcups2 libfontconfig1 libgdk-pixbuf2.0-0 libgtk-3-0 libnspr4 libpango-1.0-0 libxss1 fonts-liberation libappindicator1 libnss3 lsb-release xdg-utils
 sudo apt-get -y upgrade
 
+# INSTALL CHROME
 wget https://dl.google.com/linux/direct/google-chrome-stable_current_amd64.deb
 sudo dpkg -i --force-depends google-chrome-stable_current_amd64.deb
 sudo apt-get install -fy
 
+# INSTALL OPERA
 wget https://download3.operacdn.com/pub/opera/desktop/72.0.3815.400/linux/opera-stable_72.0.3815.400_amd64.deb
 sudo dpkg -i --force-depends opera-stable_72.0.3815.400_amd64.deb
 sudo apt-get install -fy
+
+# INSTALL FIREFOX
+sudo apt install -y firefox
 
 # UPDATE PYTHON
 sudo apt-get install -y python3-pip
 sudo pip3 install testresources
 sudo python3 -m pip install --upgrade pip setuptools wheel
-
-# # INSTALL REPO
-# git clone https://github.com/antonkurenkov/adbtc-single
-# cd adbtc-single
 
 # INSTALL DEPENDENCIES
 sudo python3 -m pip install --no-deps --force-reinstall -r requirements.txt --no-cache-dir
