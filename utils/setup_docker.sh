3af7ec69c39ed5bcbfab56fc1a07ac7a0f8a8066
antonkurenkov <a.k.a1306@gmail.com>
2020-12-14 11:24:08 +0300

    setup docker

--- utils/setup_docker.sh
+++ utils/setup_docker.sh
@@ -6,10 +6,7 @@ sudo apt-get -y install \
 	gnupg-agent \
 	software-properties-common
 sudo apt-key fingerprint 0EBFCD88
-sudo add-apt-repository \
-	"deb [arch=amd64] https://download.docker.com/linux/ubuntu \
-	$(lsb_release -cs) \
-	stable"
+sudo add-apt-repository "deb [arch=amd64] https://download.docker.com/linux/ubuntu $(lsb_release -cs) stable"
 sudo apt-get -y update
 sudo apt-get -y install docker-ce docker-ce-cli containerd.io
 sudo usermod -aG docker $(whoami)
