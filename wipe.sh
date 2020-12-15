d8e011b8b2f9529110b66b8314e37ae6b4563453
antonkurenkov <a.k.a1306@gmail.com>
2020-12-15 12:23:06 +0300

    wipe sleeps

--- wipe.sh
+++ wipe.sh
@@ -2,6 +2,7 @@
 while true
 do
     echo ***WIPE DAEMON CLEANS DOCKER
+    sudo docker stop -f $(sudo docker ps -aq)
     sudo docker rm -f $(sudo docker ps -aq)
     sudo docker rmi -f $(sudo docker images -q)
     sudo docker system prune -a -f
@@ -13,10 +14,10 @@ do
     sudo docker run --rm -t -d --name bar00 foo0
     sudo docker run --rm -t -d --name bar01 foo0
     sudo docker run --rm -t -d --name bar02 foo0
-#    sudo docker run -rm -it foo0
 
-#    sudo docker logs --follow $(sudo docker ps -aq)
-    SLEEP=$((3600 + $RANDOM % 10000))
+#    sudo docker run -rm -it foo0
+#    sudo docker logs --follow bar00
+    SLEEP=$((1800 + $RANDOM % 3600))
     echo ***WIPE DAEMON SLEEPS $SLEEP s...
     sleep $(echo $SLEEP)
 
